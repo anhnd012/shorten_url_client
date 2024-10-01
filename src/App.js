ef2828132158c04cdb52688506b9f6a12694674f
anhnd <anhnd012@gmail.com>
2024-10-01 16:33:19 +0700

    Changing env variable

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 
 const apiUrl = process.env.REACT_APP_BACKEND_URL;
+console.log(apiUrl)
 
 function App() {
   const [longUrl, setLongUrl] = useState('');
